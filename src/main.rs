ecb1f2899e2882d13b7bb3d4dfed04c452db2bc7
Khaïs COLIN <khais.colin@gmail.com>
2022-05-16 19:15:48 +0200

    Load library files

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,4 @@
-use std::rc::Rc;
+use std::{path::PathBuf, rc::Rc};
 
 use ariadne::{Color, Fmt, Label, Report, Source};
 use chumsky::{prelude::Simple, Parser};
@@ -17,6 +17,30 @@ fn main() -> Result<()> {
 
     let mut rl = rustyline::Editor::<()>::new();
     let mut env = Env::default();
+
+    println!("Loading standard library...");
+    let src = include_str!("../lib/lib.lisp");
+    let (tokens, errs) = lexer().parse_recovery(src.trim());
+    print_lex_errs(errs, src);
+    if let Some(tokens) = tokens {
+        let (atoms, errs) = parser().parse_recovery(tokens);
+        if let Some(atoms) = atoms {
+            for atom in atoms {
+                let atom = Rc::new(atom);
+                let result = Atom::eval(atom.clone(), &mut env);
+                match result {
+                    Ok(result) => {
+                        println!("{} => {}", atom, result);
+                    }
+                    Err(e) => {
+                        eprintln!("{} !! {:?}", atom, e)
+                    }
+                }
+            }
+        }
+        print_parse_errs(errs, src);
+    }
+    println!("Finished.");
 
     let histfile = &".lisphistory.txt";
     let _ = rl.load_history(histfile);
