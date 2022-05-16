de3a725708a45db43b4c3b2c124cab952c646292
Khaïs COLIN <khais.colin@gmail.com>
2022-05-16 20:24:24 +0200

    Add library func abs, foldl, foldr, list, reverse

---
+++ lib/lib.lisp
@@ -0,0 +1,20 @@
+(define (abs x) (if (< x 0) (- 0 x) x))
+
+(define (foldl proc init list)
+  (if list
+      (foldl proc
+             (proc init (car list))
+             (cdr list))
+      init))
+
+(define (foldr proc init list)
+  (if list
+      (proc (car list)
+            (foldr proc init (cdr list)))
+      init))
+
+(define (list . items)
+  (foldr cons nil items))
+
+(define (reverse list)
+  (foldl (lambda (a x) (cons x a )) nil list))
