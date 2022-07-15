73c1fb3751e0f2e642b9ecd605b0961363b32c08
Khaïs COLIN <khais.colin@gmail.com>
2022-07-15 15:14:01 +0200

    Format lib.lisp according to the new fomatter

--- lib/lib.lisp
+++ lib/lib.lisp
@@ -1,36 +1,37 @@
 (define eq? =)
 
-(define (abs x) (if (< x 0) (- 0 x) x))
+(define (abs x)
+   (if (< x 0) (- 0 x) x))
 
 (define (foldl proc init list)
-  (if list
-      (foldl proc
-             (proc init (car list))
-             (cdr list))
+   (if list
+      (foldl proc (proc init (car list)) (cdr list))
       init))
 
 (define (foldr proc init list)
-  (if list
-      (proc (car list)
-            (foldr proc init (cdr list)))
+   (if list
+      (proc (car list) (foldr proc init (cdr list)))
       init))
 
-(define (list . items)
-  (foldr cons nil items))
+(define (list . items) (foldr cons nil items))
 
 (define (reverse list)
-  (foldl (lambda (a x) (cons x a )) nil list))
+   (foldl
+      (lambda (a x) (cons x a))
+      nil
+      list))
 
 (define (unary-map proc list)
-  (foldr (lambda (x rest) (cons (proc x) rest))
-         nil
-         list))
+   (foldr
+      (lambda (x rest) (cons (proc x) rest))
+      nil
+      list))
 
 (define (map proc . arg-lists)
-  (if (car arg-lists)
-      (cons (apply proc (unary-map car arg-lists))
-            (apply 'map (cons proc
-                             (unary-map cdr arg-lists))))
+   (if (car arg-lists)
+      (cons
+         (apply proc (unary-map car arg-lists))
+         (apply (quote map) (cons proc (unary-map cdr arg-lists))))
       nil))
 
 (define (append a b) (foldr cons b a))
@@ -40,58 +41,61 @@
 (define (cadr x) (car (cdr x)))
 
 (defmacro (quasiquote x)
-  (if (pair? x)
-      (if (eq? (car x) 'unquote)
-          (cadr x)
-          (if (eq? (caar x) 'unquote-splicing)
-              (list 'append
-                    (cadr (car x))
-                    (list 'quasiquote (cdr x)))
-              (list 'cons
-                    (list 'quasiquote (car x))
-                    (list 'quasiquote (cdr x)))))
-      (list 'quote x)))
+   (if (pair? x)
+      (if (eq? (car x) (quote unquote))
+         (cadr x)
+         (if (eq? (caar x) (quote unquote-splicing))
+            (list
+               (quote append)
+               (cadr (car x))
+               (list (quote quasiquote) (cdr x)))
+            (list
+               (quote cons)
+               (list (quote quasiquote) (car x))
+               (list (quote quasiquote) (cdr x)))))
+      (list (quote quote) x)))
 
 (defmacro (let defs . body)
-  `((lambda ,(map car defs) ,@body)
-    ,@(map cadr defs)))
+   (quasiquote
+      ((lambda (unquote (map car defs)) (unquote-splicing body))
+         (unquote-splicing (map cadr defs)))))
 
 (define +
-  (let ((old+ +))
-    (lambda xs (foldl old+ 0 xs))))
+   (let
+      ((old+ +))
+      (lambda xs (foldl old+ 0 xs))))
 
 (define -
-  (let ((old- -))
-    (lambda xs (foldl old- (car xs) (cdr xs)))))
+   (let
+      ((old- -))
+      (lambda xs (foldl old- (car xs) (cdr xs)))))
 
 (define *
-  (let ((old* *))
-    (lambda xs (foldl old* (car xs) (cdr xs)))))
+   (let
+      ((old* *))
+      (lambda xs (foldl old* (car xs) (cdr xs)))))
 
 (define /
-  (let ((old/ /))
-    (lambda xs (foldl old/ (car xs) (cdr xs)))))
-
+   (let
+      ((old/ /))
+      (lambda xs (foldl old/ (car xs) (cdr xs)))))
 
 (define (last x)
-  (if (pair? x)
+   (if (pair? x)
       (if (pair? (cdr x))
-          (last (cdr x))
-          (if (= nil (cdr x))
-              (car x)
-              (cdr x)))
+         (last (cdr x))
+         (if (= nil (cdr x))
+            (car x)
+            (cdr x)))
       (x)))
 
 (define (list-length x)
-  (if (pair? x)
+   (if (pair? x)
       (+ 1 (list-length (cdr x)))
-      (if (= nil x)
-          0
-          1)))
+      (if (= nil x) 0 1)))
 
 (define (length x)
-  (if (pair? x)
+   (if (pair? x)
       (list-length x)
-      (if (string? x)
-          (string-length x)
-          nil)))
+      (if (string? x) (string-length x) nil)))
+
