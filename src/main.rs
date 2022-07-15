d9361e9fa95db07a66b8d7754c408d2cdd7f8945
Khaïs COLIN <khais.colin@gmail.com>
2022-07-15 14:51:50 +0200

    Convert to a subcommand CLI interface

--- src/main.rs
+++ src/main.rs
@@ -1,49 +1,84 @@
 use std::{fs::File, io::Read};
 
 use chumsky::Parser as _;
-use clap::Parser;
+use clap::{Parser, Subcommand};
 use color_eyre::{eyre::Context, Result};
 use gc::Gc;
 use lwhlisp::{atom::Atom, env::Env, parsing::parser, print_parse_errs};
 
 /// lwhlisp -- Lisp interpreter in Rust
 #[derive(clap::Parser, Debug)]
-#[clap(author, version, about)]
+#[clap(author, version, about, propagate_version = true)]
 struct Args {
-    /// Overide library files to evaluate at startup
-    #[clap(long)]
-    library: Vec<String>,
+    #[clap(subcommand)]
+    command: Commands,
+}
 
-    /// Files to evaluate
-    #[clap(short, long)]
-    files: Vec<String>,
+#[derive(Subcommand, Debug)]
+enum Commands {
+    /// Run a file or a REPL
+    Run {
+        /// Overide library files to evaluate at startup
+        #[clap(long)]
+        library: Vec<String>,
 
-    /// Start a REPL. Implied if no FILE is given
-    #[clap(long)]
-    repl: bool,
+        /// Files to evaluate
+        #[clap(short, long)]
+        files: Vec<String>,
 
-    /// Show debugging information in the library
-    #[clap(long)]
-    debug_library: bool,
+        /// Start a REPL. Implied if no FILE is given
+        #[clap(long)]
+        repl: bool,
 
-    /// Show debugging information in evaluated files
-    #[clap(long)]
-    debug: bool,
+        /// Show debugging information in the library
+        #[clap(long)]
+        debug_library: bool,
+
+        /// Show debugging information in evaluated files
+        #[clap(long)]
+        debug: bool,
+    },
 }
 
 fn main() -> Result<()> {
     color_eyre::install()?;
-    let mut args = Args::parse();
-    if args.files.is_empty() {
-        args.repl = true;
+    let args = Args::parse();
+
+    match args.command {
+        Commands::Run {
+            library,
+            files,
+            repl,
+            debug_library,
+            debug,
+        } => {
+            subcommand_run(library, files, repl, debug_library, debug)?;
+        }
+    }
+
+    Ok(())
+}
+
+fn subcommand_run(
+    library: Vec<String>,
+    files: Vec<String>,
+    repl: bool,
+    debug_library: bool,
+    debug: bool,
+) -> Result<(), color_eyre::Report> {
+    let mut repl = repl;
+    if files.is_empty() {
+        repl = true;
     }
 
     let mut env = Env::default();
 
-    if args.library.is_empty() {
-        args.library.push(String::from("lib/lib.lisp"));
+    let mut library = library;
+    if library.is_empty() {
+        library.push(String::from("lib/lib.lisp"));
     }
-    for library_path in args.library {
+
+    for library_path in library {
         let src = read_file_to_string(&library_path)?;
 
         let (atoms, errs) = parser().parse_recovery_verbose(src.trim());
@@ -54,7 +89,7 @@ fn main() -> Result<()> {
                 let result = Atom::eval(atom.clone(), &mut env);
                 match result {
                     Ok(result) => {
-                        if args.debug_library {
+                        if debug_library {
                             println!("{}", atom);
                             println!("=> {}", result);
                         }
@@ -66,8 +101,7 @@ fn main() -> Result<()> {
             }
         }
     }
-
-    for file in args.files {
+    for file in files {
         let src = read_file_to_string(&file)?;
         let (atoms, errs) = parser().parse_recovery_verbose(src.trim());
         print_parse_errs(errs, src.trim());
@@ -77,7 +111,7 @@ fn main() -> Result<()> {
                 let result = Atom::eval(atom.clone(), &mut env);
                 match result {
                     Ok(result) => {
-                        if args.debug {
+                        if debug {
                             println!("{}", atom);
                             println!("=> {}", result);
                         }
@@ -90,14 +124,13 @@ fn main() -> Result<()> {
         }
     }
 
-    if args.repl {
-        repl(env)?;
+    if repl {
+        run_repl(env)?;
     }
-
     Ok(())
 }
 
-fn repl(mut env: Env) -> Result<(), color_eyre::Report> {
+fn run_repl(mut env: Env) -> Result<(), color_eyre::Report> {
     let mut rl = rustyline::Editor::<()>::new();
     let histfile = &".lisphistory.txt";
     let _ = rl.load_history(histfile);
